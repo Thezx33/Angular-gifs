f0184a3173d88a3af00f9a6db9270d2aee9bcfdb
zxzx <oscarmiguelayalaruelas@gmail.com>
2021-03-29 15:42:21 -0700

    todo menos las animaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
@@ -14,7 +14,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpClientModule,
     SharedModule,
-    GifsModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,28 +8,46 @@ import { HttpClient } from '@angular/common/http';
 export class GifsService {
 
   private apiKey: string = '5PgTUJMm50sNFD9yzkSbN9Wb0vlasKt5';
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
+  public resultados: Gif[] = [];
 
   // Obtener replica del arreglo del historial
   get historial() {
     return [ ...this._historial ];
   }
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient ) {
+    this._historial = JSON.parse( localStorage.getItem( 'historial' ) ) || [];
+    this.resultados = JSON.parse( localStorage.getItem( 'resultado' ) ) || [];
+  }
 
   // Añadir busqueda al historial
-  async buscarGifs( query: string ) {
+  buscarGifs( query: string = '' ) {
     
     query = query.trim().toLocaleLowerCase();
 
     if ( !this.historial.includes( query )) {
       this._historial.unshift( query );
       this._historial = this._historial.splice( 0, 10 );
+
+      localStorage.setItem( 'historial', JSON.stringify( this._historial ) );
+
     }
     
-    const resp = await fetch('api.giphy.com/v1/gifs/search?api_key=R0CafjnSIuiMF7cYj3f28fT0VSZCLW6W&q=dragon ball z&limit=10');
-    console.log( resp );
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+    
+
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params }).subscribe( ( resp ) => {
+      console.log( resp.data );
+
+      this.resultados = resp.data;
+      localStorage.setItem( 'resultado', JSON.stringify( this.resultados ) );
+    });
     
     
 
